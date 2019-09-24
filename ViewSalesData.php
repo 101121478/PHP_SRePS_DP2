e2971ee122f51ed1b4aef5b5aeb4d454631abfc8
Luke Parker <101121478@student.swin.edu.au>
2019-09-24 15:18:06 +1000

    Added Basic Web GUI
    -Added Home page
    - Added AddSalesData page
    - Added EditSalesData page
    - Added ViewSalesData page

---
+++ ViewSalesData.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>SRePS - View Sales Data Page</title>
+</head>
+
+<body>
+	<h1>View Sales Data Page</h1>
+
+</body>
+</html>
